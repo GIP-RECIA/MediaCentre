ae4ce4cc25deee63b498be0cb7e2dbd304fce7f5
Anaël iMac <anael@barodine.fr>
2021-06-29 16:07:49 +0200

    fix: reimplemented UserResourceImpl with HttpServletRequest requests

--- src/main/java/org/esco/portlet/mediacentre/dao/impl/UserResourceImpl.java
+++ src/main/java/org/esco/portlet/mediacentre/dao/impl/UserResourceImpl.java
@@ -15,34 +15,38 @@
  */
 package org.esco.portlet.mediacentre.dao.impl;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apereo.portal.soffit.Headers;
 import org.esco.portlet.mediacentre.dao.IUserResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+@Slf4j
 @Service
 @Profile("!mock")
-public class UserResourceImpl implements IUserResource{
+public class UserResourceImpl implements IUserResource {
+    // Attributes
+    @Value("${security-configuration.soffit.jwt.signatureKey:Changeme}")
+    private String signatureKey;
 
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
-
-    //private IUserResource mockedUserResource;
-    
+    // Methods
+    /*
     /**
      * Retrieve the user info attribute from portlet context, or the Mocked user info
      *
      * @param request the portlet request
      * @param attributeName the attribute to retrieve
      * @return the user info attribute values
-     */
+     * /
     @SuppressWarnings("unchecked")
     public List<String> getUserInfo(@NonNull final HttpServletRequest request, @NonNull final String attributeName) {
     	log.debug("getUserInfo attributeName={}", attributeName);
@@ -70,13 +74,15 @@ public class UserResourceImpl implements IUserResource{
         log.debug("getUserInfo attributeValues = {}", attributeValues );
         return attributeValues;
     }
+    */
 
+    /*
     /**
      * Retrieve the user info Map from portlet context, or the Mocked user info
      *
      * @param request the portlet request
      * @return  the user info map with association attribute values
-     */
+     * /
     @SuppressWarnings("unchecked")
     public Map<String, List<String>> getUserInfoMap(@NonNull final HttpServletRequest request) {
 
@@ -92,12 +98,71 @@ public class UserResourceImpl implements IUserResource{
         return new LinkedHashMap<>();
 
     }
+    */
 
-    /**public IUserResource getMockedUserResource() {
-        return mockedUserResource;
+    @Override
+    public List<String> getUserInfo(@NonNull final HttpServletRequest request, @NonNull final String attributeName) {
+        log.debug("getUserInfo attributeName={}", attributeName);
+        return this.getUserInfoMap(request).getOrDefault(attributeName, new ArrayList<>());
     }
 
-    public void setMockedUserResource(IUserResource mockedUserResource) {
-        this.mockedUserResource = mockedUserResource;
-    }*/
+    @Override
+    public Map<String, List<String>> getUserInfoMap(@NonNull final HttpServletRequest request) {
+        final Jws<Claims> oidcToken = parseOidcToken(request);
+
+        Map<String, List<String>> userInfo = new LinkedHashMap<>();
+
+        if (oidcToken != null) {
+            for (String claimName : oidcToken.getBody().keySet()) {
+                final Object claimValue = oidcToken.getBody().get(claimName);
+                final List<String> claimValueList = new ArrayList<>();
+                userInfo.put(claimName, claimValueList);
+
+                if (claimValue == null) {
+                    log.debug("claim value of claim name {} is null", claimName);
+                } else if (claimValue instanceof List<?>) {
+                    for (Object claimValueElement : (List<?>) claimValue) {
+                        claimValueList.add(claimValueElement.toString());
+                    }
+                } else {
+                    claimValueList.add(claimValue.toString());
+                }
+            }
+        }
+
+        return userInfo;
+    }
+
+    /**
+     * Obtains information about the user from the Authorization header in the form of an OIDC Id
+     * token.  In the future, it would be better to provide a standard tool (bean) for this job in
+     * the <code>uPortal-soffit-renderer</code> component.
+     */
+    private Jws<Claims> parseOidcToken(HttpServletRequest req) {
+
+        final String authHeader = req.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader.isBlank() || !authHeader.startsWith(Headers.BEARER_TOKEN_PREFIX)) {
+            /*
+             * No OIDC token available
+             */
+            return null;
+        }
+
+        final String bearerToken = authHeader.substring(Headers.BEARER_TOKEN_PREFIX.length());
+
+        try {
+            // Validate & parse the JWT
+            final Jws<Claims> rslt = Jwts.parserBuilder().setSigningKey(this.signatureKey).build().parseClaimsJws(bearerToken);
+
+            log.debug("Found the following OIDC Id token:  {}", rslt.toString());
+
+            return rslt;
+        } catch (Exception e) {
+            log.info("The following Bearer token is unusable:  '{}'", bearerToken);
+            log.debug("Failed to validate and/or parse the specified Bearer token", e);
+        }
+
+        return null;
+
+    }
 }
